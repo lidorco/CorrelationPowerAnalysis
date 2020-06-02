02a941b4018caae08ca4cb2bf36636455a54bb0c
lidorco <lidorco@outlook.com>
2020-06-02 20:33:52 +0300

    Print results as excepted

--- M1/src/ex02_M1.py
+++ M1/src/ex02_M1.py
@@ -4,39 +4,36 @@ from statistics import variance, mean
 
 import requests
 
-USER_NAME = 'name'
+USER_NAME = 'test'
 DIFFICULTY = 1
 SERVER_URL = "http://aoi.ise.bgu.ac.il/encrypt?user={user_name}&difficulty={difficulty}/"
 
 NUMBER_OF_TRACE_TO_DOWNLOAD = 100
+
 
 def get_trace():
     response =  requests.get(SERVER_URL.format(user_name= USER_NAME, difficulty=DIFFICULTY))
     return json.loads(response.content)
 
 
-def save_to_file(file_name, result):
-    with open(file_name, 'w') as f:
-        f.write(json.dumps(result).replace('],', '],\n'))
-
-
-def create_empty_json_file(file_name):
-    with open(file_name, 'w') as f:
-        f.write('{}')
+def append_to_file(file_name, trace):
+    with open(file_name, 'a') as f:
+        f.write("{}\n".format(trace).replace("'", '"'))
 
 
 def main():
-    print("file to save trace : {}".format(sys.argv[1]))
-    create_empty_json_file(sys.argv[1])
+    out_file = sys.argv[1]
     result = dict()
+
     while len(result.keys()) < NUMBER_OF_TRACE_TO_DOWNLOAD:
         trace = get_trace()
         result[trace['plaintext']] = trace['leaks']
+        append_to_file(out_file, trace)
 
-    save_to_file(sys.argv[1], result)
     print("Mean\tVariance")
+
     for plain_text, leaks in result.items():
-        print("{}\t{}".format(mean(leaks), variance(leaks)))
+        print("%.2f\t%.2f" % (mean(leaks), variance(leaks)))
 
 
 
